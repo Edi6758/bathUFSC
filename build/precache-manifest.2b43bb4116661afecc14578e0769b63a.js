e77e231dd9e0d8551bb5a519aade5289c9c285c7
Edi6758 <ediarts7@gmail.com>
2022-12-16 03:12:42 -0300

    foi

---
+++ build/precache-manifest.2b43bb4116661afecc14578e0769b63a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3b55d1a0de63fb504d8f7b9a4f6cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e84768cf0522cc7ad249",
+    "url": "/static/css/4.bee358b8.chunk.css"
+  },
+  {
+    "revision": "b123effe3378abfb0acc",
+    "url": "/static/css/main.05dc03bc.chunk.css"
+  },
+  {
+    "revision": "4c0e55399e88f758affc",
+    "url": "/static/js/2.aa75691e.chunk.js"
+  },
+  {
+    "revision": "3c669038cd8e0c6c76fb",
+    "url": "/static/js/3.abf0e07c.chunk.js"
+  },
+  {
+    "revision": "775c358052fbfe1ffc80c0f137ad8d47",
+    "url": "/static/js/3.abf0e07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84768cf0522cc7ad249",
+    "url": "/static/js/4.bd8a4a73.chunk.js"
+  },
+  {
+    "revision": "b123effe3378abfb0acc",
+    "url": "/static/js/main.bbdc55e8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.bbdc55e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40794571ee8f93546d6c",
+    "url": "/static/js/runtime-main.370dd132.js"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "531cc6b1d07d10968a88635babcdfaa2",
+    "url": "/static/media/dogs-footer.531cc6b1.svg"
+  },
+  {
+    "revision": "531cc6b1d07d10968a88635babcdfaa2",
+    "url": "/static/media/dogs.531cc6b1.svg"
+  },
+  {
+    "revision": "57fa52ac62a11268389f02f840f1ab8c",
+    "url": "/static/media/enviar.57fa52ac.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "66cf14f3dcfe113818de5dd8aaacee9c",
+    "url": "/static/media/login.66cf14f3.jpg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
